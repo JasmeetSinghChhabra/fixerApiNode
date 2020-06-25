c9ce4eb997a1f1c9ba8bc34e5f79b272e3e2605c
Jasmeet Singh Chhabra <js.chhabra1994@gmail.com>
2020-06-25 13:49:34 -0700

    Done

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require("path");
 const express = require("express");
 const axios = require("axios");
 const { SSL_OP_EPHEMERAL_RSA } = require("constants");
-const fetchUrl = require("fetch").fetchUrl;
+// const fetchUrl = require("fetch").fetchUrl;
+const fetch = require("node-fetch");
 
 const app = express();
 
@@ -12,13 +13,15 @@ app.use("/api", (req, res, next) => {
   let amount = parseInt(req.query.amount, 10);
   let base = req.query.base;
   let target = req.query.target;
+  let convertedRates = [];
+  let url = "http://data.fixer.io/api/";
+  let API_KEY = "f5a9c4be0416317dcdeffc0dad390909";
 
   console.log("Date : " + date);
   console.log("Amount : " + amount);
   console.log("Base : " + base);
   console.log("Target : " + target);
-  let url = "http://data.fixer.io/api/";
-  let API_KEY = "f5a9c4be0416317dcdeffc0dad390909";
+
   url =
     url +
     date +
@@ -35,36 +38,32 @@ app.use("/api", (req, res, next) => {
     }
   }
 
-  convertedRates = [];
-
-  function getPosition(string, subString, index) {
-    return string.split(subString, index).join(subString).length;
-  }
-  fetchUrl(url, function (error, meta, body) {
-    let ng = body.toString();
-    let start = getPosition(ng, "{", 2);
-    let end = ng.indexOf("}");
-    let rates = ng.substring(start + 1, end).split(",");
-    console.log(rates.length);
-    rates.forEach((element) => {
-      let check = element.replace('"', "").replace('"', "").split(":");
-      // console.log("Check : " + check[1]);
-      convertedRates.push(
-        new Convertedrates(check[0], parseFloat(check[1], 10) * amount)
-      );
-    });
-  });
-  convertedRates.forEach((e) => console.log("Data  :   " + e));
   axios
     .get(url)
     .then(function (response) {
+      //
+      let data = response.data.rates;
+      var sd = JSON.stringify(data);
+      console.log(sd);
+      let start = sd.indexOf("{");
+      let end = sd.indexOf("}");
+      let rates = sd.substring(start + 1, end).split(",");
+      console.log(rates);
+      console.log(rates.length);
+      rates.forEach((element) => {
+        let check = element.replace('"', "").replace('"', "").split(":");
+        convertedRates.push(
+          new Convertedrates(check[0], parseFloat(check[1], 10) * amount)
+        );
+      });
+      //
       let result = {
         success: response.data.success,
         timestamp: response.data.timestamp,
         historical: response.data.historical,
         base: response.data.base,
         date: response.data.date,
-        convertedRates,
+        rates: convertedRates,
       };
       console.log(result);
       res.send(result);
