96bc64d885e4ce68b9bf516149eb3acfe28bec36
Jasmeet Singh Chhabra <js.chhabra1994@gmail.com>
2020-06-25 09:38:55 -0700

    Bootstrapping Complete

--- 2.js
+++ 2.js
@@ -29,41 +29,6 @@ function checkBase(base) {
   }
 }
 //data.fixer.io/api/2013-12-24?access_key=f5a9c4be0416317dcdeffc0dad390909&base=EUR&symbols=USD,CAD,EUR
-rl.question("Enter Date in YYYY-MM-DD ? ", function (date) {
-  let url = "http://data.fixer.io/api/";
-  //2013-12-24?
-  let access_key = "?access_key=f5a9c4be0416317dcdeffc0dad390909&base=";
-  console.log("date" + date);
-  url = url + date + access_key;
-  console.log("url" + url);
-  rl.question("Enter Base ? ", function (base) {
-    base = checkBase(base);
-    console.log("base" + base);
-    url = url + base;
-    console.log("url" + url);
-    rl.question("Enter Target ? ", function (target) {
-      url = url + "&symbols=" + target;
-      console.log(`base is ${base}, Target Currency is ${target}`);
-      console.log("url" + url);
-      rl.question("Enter Conversion Amount ? ", function (conversionAmount) {
-        console.log("Final: " + url);
 
-        rl.close();
-      });
-    });
-  });
-});
 console.log("url" + url);
-axios
-  .get(url)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 // server.js
 const path = require("path");
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.use("/api", (req, res, next) => {
+  var date = req.query.date;
   var amount = req.query.amount;
   var base = req.query.base;
   var target = req.query.target;
 
+  console.log("Date : " + date);
   console.log("Amount : " + amount);
   console.log("Base : " + base);
   console.log("Target : " + target);
+  let url = "http://data.fixer.io/api/";
+  let access_key = "?access_key=f5a9c4be0416317dcdeffc0dad390909&base=";
+  url = url + date + access_key + base + "&symbols=" + target;
+
+  axios
+    .get(url)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 });
 app.use("/", express.static("public"));
 
